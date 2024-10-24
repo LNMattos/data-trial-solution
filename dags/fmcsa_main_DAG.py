fff79c7b8108af2216d39954ae6243ca1a3bbbb7
LNMattos <leonardomattus@gmail.com>
2024-10-24 00:57:25 -0400

    minor adjusts fmcsa dag

--- dags/fmcsa_main_DAG.py
+++ dags/fmcsa_main_DAG.py
@@ -2,15 +2,16 @@ import os
 import yaml
 from airflow import DAG
 from airflow.operators.python import PythonOperator
+from airflow.operators.empty import EmptyOperator
 from datetime import datetime
-from pipelines.fmcsa_companies.process import process_data as process_fmcsa_companies
-from pipelines.fmcsa_company_snapshot.process import process_data as process_fmcsa_company_snapshot
-from pipelines.fmcsa_complaints.process import process_data as process_fmcsa_complaints
-from pipelines.fmcsa_safer_data.process import process_data as process_fmcsa_safer_data
+from pipelines.fmcsa_companies.process import process_pipeline as pipeline_fmcsa_companies
+from pipelines.fmcsa_company_snapshot.process import process_pipeline as pipeline_fmcsa_company_snapshot
+from pipelines.fmcsa_complaints.process import process_pipeline as pipeline_fmcsa_complaints
+from pipelines.fmcsa_safer_data.process import process_pipeline as pipeline_fmcsa_safer_data
 
 default_args = {
     'owner': 'leo.mattos',
-    'depends_on_past': False,
+    'depends_on_past': False
 }
 
 def load_config(pipeline_name):
@@ -29,20 +30,23 @@ def run_pipeline(pipeline_name, process_function, **kwargs):
 
 with DAG(
     'fmcsa_main_dag',
+    tags=['FMCSA', 'S3', 'POSTGRESQL'],
     default_args=default_args,
-    description='DAG para processar dados diversos da FMCSA',
+    description='DAG to process FMCSA data',
     schedule_interval='@daily',
     start_date=datetime(2024, 1, 1),
     catchup=False,
     max_active_runs=1
 ) as dag:
+    start_task = EmptyOperator(task_id='Start', dag=dag)
+    finish_task = EmptyOperator(task_id='Finish', dag=dag)
 
     process_fmcsa_companies_task = PythonOperator(
         task_id='process_fmcsa_companies',
         python_callable=run_pipeline,
         op_kwargs={
             'pipeline_name': 'fmcsa_companies',
-            'process_function': process_fmcsa_companies
+            'process_function': pipeline_fmcsa_companies
         },
     )
 
@@ -51,7 +55,7 @@ with DAG(
         python_callable=run_pipeline,
         op_kwargs={
             'pipeline_name': 'fmcsa_company_snapshot',
-            'process_function': process_fmcsa_company_snapshot
+            'process_function': pipeline_fmcsa_company_snapshot
         },
     )
 
@@ -60,7 +64,7 @@ with DAG(
         python_callable=run_pipeline,
         op_kwargs={
             'pipeline_name': 'fmcsa_complaints',
-            'process_function': process_fmcsa_complaints
+            'process_function': pipeline_fmcsa_complaints
         },
     )
 
@@ -69,14 +73,12 @@ with DAG(
         python_callable=run_pipeline,
         op_kwargs={
             'pipeline_name': 'fmcsa_safer_data',
-            'process_function': process_fmcsa_safer_data
+            'process_function': pipeline_fmcsa_safer_data
         },
     )
 
-    # Definir a ordem das tasks, se necessário
-    # Exemplo: process_fmcsa_companies_task >> process_fmcsa_company_snapshot_task >> process_fmcsa_complaints_task >> process_fmcsa_safer_data_task
-
-    # Caso não haja dependências específicas, as tasks podem ser executadas em paralelo
-    process_fmcsa_companies_task >> process_fmcsa_company_snapshot_task
-    process_fmcsa_company_snapshot_task >> process_fmcsa_complaints_task
-    process_fmcsa_complaints_task >> process_fmcsa_safer_data_task
+    start_task >> (
+    process_fmcsa_companies_task,
+    process_fmcsa_company_snapshot_task,
+    process_fmcsa_complaints_task,
+    process_fmcsa_safer_data_task) >> finish_task
